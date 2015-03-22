9d6685fe7d1c089e7cfea512a840b8d22f953ed1
Anders D. Johnson <anders.d.johnson.developer@gmail.com>
2015-03-22 06:42:15 -0500

    init

---
+++ README.md
@@ -0,0 +1,67 @@
+# maven-executable
+
+
+## Usage
+
+Ideally:
+
+```
+mvnx org.apache.ant:ant
+```
+
+
+## Features
+
+* Auto-install
+* Search?
+
+
+## Research
+
+```
+mvn dependency:copy "-Dartifact=org.apache.ant:ant:RELEASE" "-DoutputDirectory=./bin"
+```
+
+```
+mvn dependency:get "-Dartifact=org.apache.ant:ant:RELEASE"
+```
+
+```
+mvn -f "C:\Users\Anders\.m2\repository\com\google\guava\guava\18.0\guava-18.0.pom" org.apache.maven.plugins:maven-dependency-plugin:2.10:build-classpath
+```
+
+```
+mvn org.apache.maven.plugins:maven-dependency-plugin:2.10:build-classpath "-Dmdep.outputFile=foo"
+```
+
+```
+mvn -f .\mvn help:effective-pom
+```
+
+
+* https://mizdebsk.fedorapeople.org/xmvn/
+* https://stackoverflow.com/questions/10536221/fetching-maven-artifacts-programmatically
+* http://aether.jcabi.com/
+* https://maven.apache.org/ref/3.0.3/maven-core/apidocs/org/apache/maven/repository/RepositorySystem.html
+* https://maven.apache.org/ref/3.2.5/maven-artifact/apidocs/org/apache/maven/artifact/repository/ArtifactRepository.html
+* https://maven.apache.org/ref/3.2.5/maven-artifact/apidocs/org/apache/maven/artifact/repository/ArtifactRepository.html#getMirroredRepositories()
+* https://maven.apache.org/ref/3.2.5/maven-artifact/apidocs/org/apache/maven/artifact/repository/ArtifactRepository.html#find(org.apache.maven.artifact.Artifact)
+* https://maven.apache.org/ref/3.2.5/maven-artifact/apidocs/org/apache/maven/artifact/Artifact.html
+* https://stackoverflow.com/questions/4381460/get-mavenproject-from-just-the-pom-xml-pom-parser
+* http://sonatype.github.io/sonatype-aether/apidocs/org/sonatype/aether/repository/LocalRepository.html
+* https://rolfje.wordpress.com/2013/07/18/non-java-binary-dependencies-in-maven/
+* http://mojo.codehaus.org/exec-maven-plugin/index.html
+* http://www.petrikainulainen.net/programming/tips-and-tricks/creating-a-runnable-binary-distribution-with-maven-assembly-plugin/
+* https://stackoverflow.com/questions/2022032/building-a-runnable-jar-with-maven-2
+* https://stackoverflow.com/questions/574594/how-can-i-create-an-executable-jar-with-dependencies-using-maven
+* http://mojo.codehaus.org/exec-maven-plugin/
+* http://maven.40175.n5.nabble.com/Can-mvn-install-packages-globally-e-g-command-line-tools-like-nutch-td5767828.html
+* https://maven.apache.org/guides/mini/guide-using-one-source-directory.html
+* http://blog.sonatype.com/2010/01/how-to-create-two-jars-from-one-project-and-why-you-shouldnt/
+* https://github.com/cstamas/maven-indexer-examples
+* https://stackoverflow.com/questions/2063378/how-can-i-get-the-list-of-artifacts-from-my-maven-repository
+* https://github.com/kumarshantanu/lein-localrepo
+* https://maven.apache.org/guides/plugin/guide-java-plugin-development.html
+* http://books.sonatype.com/mvnref-book/reference/writing-plugins-sect-custom-plugin.html
+* http://docs.codehaus.org/display/MAVENUSER/Mojo+Developer+Cookbook
+* http://blog.sonatype.com/2011/01/how-to-use-aether-in-maven-plugins/
---
+++ src/main/groovy/me/andrz/maven/executable/MavenExecutable.groovy
@@ -0,0 +1,124 @@
+package me.andrz.maven.executable
+
+import com.jcabi.aether.Aether
+import groovy.util.logging.Slf4j
+import org.sonatype.aether.artifact.Artifact
+import org.sonatype.aether.repository.RemoteRepository
+import org.sonatype.aether.util.artifact.DefaultArtifact
+
+import java.util.jar.JarFile;
+
+/**
+ *
+ */
+@Slf4j
+class MavenExecutable {
+
+    public static final String defaultVersion = 'RELEASE'
+
+    public static final File cwd = new File(System.getProperty("user.dir"))
+
+    public static void main(String[] args) {
+        log.debug args.toString()
+        run(args[0])
+    }
+
+    /**
+     *
+     * @param coords <groupId>:<artifactId>[:<extension>[:<classifier>]]:<version>
+     */
+    public static Process run(String coords, String mainClassName = null) {
+
+        if (! coords) {
+            throw new RuntimeException("Must provide <artifact> argument.")
+        }
+
+        coords = sanitizeCoords(coords)
+
+        Aether aether = new Aether(getRemotes(), getLocal())
+        Artifact targetArtifact = new DefaultArtifact(coords)
+        Collection<Artifact> artifacts = aether.resolve(targetArtifact, "runtime")
+
+        List<File> classpaths = []
+
+        def targetJarFile = null
+
+        artifacts.each { artifact ->
+            if (artifactsMatch(targetArtifact, artifact)) {
+                targetJarFile = artifact.file
+            }
+            classpaths.add(artifact.file)
+        }
+
+        log.debug "targetJarFile: $targetJarFile"
+
+        if (! targetJarFile) {
+            throw new RuntimeException("Could not resolve JAR for \"$coords\"")
+        }
+
+        if (! mainClassName) {
+            mainClassName = getMainClassName(targetJarFile)
+        }
+
+        log.debug "main: $mainClassName"
+
+        def classpath = classpaths.join(';')
+
+        def env = System.getenv()
+        def envStr = toEnvStrings(env)
+        def command = "java -cp \"${classpath}\" ${mainClassName}"
+
+        log.debug "command: ${command}"
+        log.debug "envStr: ${envStr}"
+
+        def proc = command.execute(envStr, cwd)
+
+        return proc
+    }
+
+    /**
+     * https://maven.apache.org/pom.html#Maven_Coordinates
+     *
+     * @param coords
+     * @return
+     */
+    public static String sanitizeCoords(String coords) {
+        if (coords.split(':').length < 3) {
+            coords += ':' + defaultVersion
+        }
+        return coords
+    }
+
+    public static boolean artifactsMatch(Artifact a, Artifact b) {
+        return a.artifactId == b.artifactId && a.groupId == b.groupId
+    }
+
+    public static String getMainClassName(File jarFile) {
+        JarFile jf = new JarFile(jarFile);
+        String mainClassName = jf?.manifest?.mainAttributes?.getValue("Main-Class")
+        return mainClassName
+    }
+
+    public static String[] toEnvStrings(def env) {
+        return env.collect { k, v -> "$k=$v" }
+    }
+
+    public static File getLocal() {
+        Map<String, String> env = System.getenv();
+        String m2home = env.get("M2_HOME");
+        log.debug "M2_HOME: ${m2home}"
+
+        return new File(m2home);
+    }
+
+    public static Collection<RemoteRepository> getRemotes() {
+        return Arrays.asList(
+                new RemoteRepository(
+                        "maven-central",
+                        "default",
+                        "http://repo1.maven.org/maven2/"
+                )
+        );
+    }
+
+}
