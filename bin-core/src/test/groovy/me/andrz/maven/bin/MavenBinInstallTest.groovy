d25574d3d0eb6382b9347e9d3089b33999eda976
Anders D. Johnson <anders.d.johnson.developer@gmail.com>
2015-03-29 00:37:57 -0500

    core test install

--- bin-core/src/test/groovy/me/andrz/maven/bin/MavenBinInstallTest.groovy
+++ bin-core/src/test/groovy/me/andrz/maven/bin/MavenBinInstallTest.groovy
@@ -1,6 +1,7 @@
 package me.andrz.maven.bin
 
 import groovy.util.logging.Slf4j
+import org.junit.Test
 
 /**
  *
@@ -8,6 +9,16 @@ import groovy.util.logging.Slf4j
 @Slf4j
 class MavenBinInstallTest {
 
-    // TODO: Add tests
+    @Test
+    public void test() {
+        MavenBin mavenBin = new MavenBin()
+
+        def artifact = 'org.apache.ant:ant'
+
+        mavenBin.run(artifact, new MavenBinParams(
+                install: true,
+                run: false
+        ))
+    }
 
 }
