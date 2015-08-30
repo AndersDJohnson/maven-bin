11c3434a917fe89fa2496dbe665a5ef9b4ca83df
Anders D. Johnson <anders.d.johnson.developer@gmail.com>
2015-08-30 17:16:46 -0500

    Add toString to Params class.

--- bin-core/src/main/groovy/me/andrz/maven/bin/MavenBinParams.groovy
+++ bin-core/src/main/groovy/me/andrz/maven/bin/MavenBinParams.groovy
@@ -1,11 +1,13 @@
 package me.andrz.maven.bin
 
 import groovy.transform.AutoClone
+import groovy.transform.ToString
 import org.eclipse.aether.artifact.Artifact
 
 /**
  *
  */
+@ToString(includeNames=true)
 @AutoClone
 class MavenBinParams {
     List<Artifact> artifacts
