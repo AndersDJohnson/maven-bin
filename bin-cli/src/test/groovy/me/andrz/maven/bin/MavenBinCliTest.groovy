521f305f384f342d7e160e3a60541a790b38c84b
Anders D. Johnson <anders.d.johnson.developer@gmail.com>
2015-08-30 20:15:33 -0500

    Add CLI main tests.

--- bin-cli/src/test/groovy/me/andrz/maven/bin/MavenBinCliTest.groovy
+++ bin-cli/src/test/groovy/me/andrz/maven/bin/MavenBinCliTest.groovy
@@ -10,8 +10,44 @@ import org.junit.Test;
 public class MavenBinCliTest {
 
     @Test
-    public void test() {
+    public void testMainSuccess() {
+        exec('org.apache.ant:ant -h')
+    }
+
+    @Test
+    public void testMainHelp() {
+        exec('-help')
+    }
+
+    @Test
+    public void testMain() {
+        exec('org.apache.ant:ant')
+    }
+
+    @Test
+    public void testInstall() {
+        exec('-i org.apache.ant:ant')
+    }
+
+    @Test
+    public void testInstallAlias() {
+        exec('-i -a ant org.apache.ant:ant')
+    }
+
+    @Test
+    public void testRun() {
         MavenBinCli.run('org.apache.ant:ant')
+    }
+
+    public void exec(String string) {
+        try {
+            MavenBinCli.main(arrayify(string))
+        } catch (MavenBinExitValueNonZeroException e) {
+        }
+    }
+
+    public String[] arrayify(String string) {
+        return string.split(' ')
     }
 
 }
